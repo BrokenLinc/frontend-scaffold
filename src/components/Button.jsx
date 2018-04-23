03d1791850016c82927d37c68ad6e9d54b9e4ff6
Lincoln Anderson <landerson@352inc.com>
2018-04-23 01:06:01 -0400

    Rename buttons for humans.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = () => (
-  <button className="btn btn-green">Button</button>
+  <button className="green-button">Button</button>
 );
 
 export default Button;
--- src/components/SourceViewer.jsx
+++ src/components/SourceViewer.jsx
@@ -25,7 +25,7 @@ class SourceViewer extends Component {
             {sourceHtml}
           </code>
         </pre>
-        <button className="btn" type="button" onClick={() => clipboard(sourceHtml)}>Copy HTML</button>
+        <button className="ghost-button" type="button" onClick={() => clipboard(sourceHtml)}>Copy HTML</button>
       </div>
     );
   }
